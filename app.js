2a7a8020321d737517c3028b7969a7c0bf525762
ibragimjon <ibrohimbiznesmen@gmail.con>
2022-02-27 12:04:28 +0500

    yosh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const sim = prompt('Ismingizni kiriting')
 const yosh = prompt('Yoshingizni kiriting')
-alert(`${sim} siz ${2022 - yosh} yili tug'ilgansiz. Siz ${12*yosh} oy, ${4*12*yosh} hafta, ${365 * yosh} kun, ${365 * 24*14} soat, ${365 * 24*14*60} minut, ${365 * 24*14*60*60} second,`)
+alert(`${sim} siz ${2022 - yosh} yili tug'ilgansiz. Siz ${12*yosh} oy, ${4*12*yosh} hafta, ${365 * yosh} kun, ${365 * 24*14} soat, ${365 * 24*14*60} minut, ${365 * 24*14*60*60} secund yashagansiz`)
 
 console.log(sim)
